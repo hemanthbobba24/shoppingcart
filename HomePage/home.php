62ab6296d475396747bf4233381df5161fff2402
Hemanth Sriram <101417955+hemanthbobba24@users.noreply.github.com>
2022-10-22 12:44:05 +0530

    Add files via upload

---
+++ HomePage/home.php
@@ -0,0 +1,22 @@
+<?php
+include('../partials/partials.php');
+?>
+	<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="footwear2.css">
+    <title>footwear</title>
+  </head>
+  <body>
+  <div class="img-container">
+        <div class="caption">
+          <h1>Fashion</h1>
+          <p>Make Your Own PATH</p>
+          <a href="../DisplayProducts/displayproducts.php" class="explore">EXPLORE</a>
+        </div>
+     </div>
+  </body>
+  </html>
